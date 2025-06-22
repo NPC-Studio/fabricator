695269c77fa7ad93cda969d01bb64f0efbf10881
kyren <kyren@kyju.org>
2025-06-21 22:52:22 -0600

    Add a generic numerical test for support maps
    Simple stochastic test to make sure that the support map won't return
    support points with larger dot products when given other normal vectors.

--- crates/collision/src/tests.rs
+++ crates/collision/src/tests.rs
@@ -1,6 +1,6 @@
-use fabricator_math::Vec2;
+use fabricator_math::{Box2, Vec2};
 
-use crate::{gjk, simplex::Simplex, support_ext::SupportMapExt, support_maps};
+use crate::{gjk, simplex::Simplex, support::SupportMap, support_ext::SupportMapExt, support_maps};
 
 #[test]
 fn test_gjk_point_circle() {
@@ -102,4 +102,40 @@ fn test_gjk_line_ellipse() {
         ),
         gjk::Result::Touching
     ));
+}
+
+#[test]
+fn test_support_maps_find_furthest() {
+    fn test_support_map<S: SupportMap<f64>>(s: S) {
+        // For the provided support map, try to ensure that points returned from S::support_point
+        // for various normal vectors have the largest dot product in comparison to points returned
+        // from S::support_point with the same normal vector rotated by various offsets.
+
+        for dir in (0..50).map(|i| Vec2::new(1.0, 0.0).rotate_angle(0.63 * i as f64)) {
+            let support_point = s.support_point(dir);
+            for rot_angle in (0..20).map(|i| 0.001 * 1.55f64.powi(i)) {
+                for rot_angle in [rot_angle, -rot_angle] {
+                    let other_dir = dir.rotate_angle(rot_angle);
+                    let other_support_point = s.support_point(other_dir);
+                    assert!(support_point.point.dot(dir) >= other_support_point.point.dot(dir));
+                }
+            }
+        }
+    }
+
+    test_support_map(support_maps::Point(Vec2::splat(2.5)));
+    test_support_map(support_maps::Line([Vec2::splat(-2.5), Vec2::splat(3.0)]));
+    test_support_map(support_maps::Line([Vec2::splat(-2.5), Vec2::splat(3.0)]));
+    test_support_map(support_maps::Circle {
+        center: Vec2::new(1.7, 1.9),
+        radius: 1.0,
+    });
+    test_support_map(support_maps::Ellipse {
+        center: Vec2::new(-1.7, 1.5),
+        radius: Vec2::new(7.0, 2.0),
+    });
+    test_support_map(support_maps::AABox(Box2::with_size(
+        Vec2::new(-5.0, -3.0),
+        Vec2::new(10.0, 7.0),
+    )));
 }
